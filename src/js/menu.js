3dcfc8686a476abd6af2f7c4ed1313520a79c713
Filipe Maia <seuemail@dominio.com>
2025-05-10 16:54:25 -0300

    ajuste da responsividade

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,15 +1,6 @@
 const menuToggle = document.querySelector('.menu-toggle');
 const nav = document.querySelector('.nav');
 
-// Clicar no botão hamburguer ativa o menu
 menuToggle.addEventListener('click', () => {
   nav.classList.toggle('active');
-});
-
-// Clicar em qualquer link do menu fecha o menu
-const navLinks = document.querySelectorAll('.nav a');
-navLinks.forEach(link => {
-  link.addEventListener('click', () => {
-    nav.classList.remove('active');
-  });
 });
